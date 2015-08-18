a25d13886f583602be14f98723b9882e11bf4c77
Jerry Dunmire <jerry@home>
2015-08-17 21:33:41 -0700

    Use deep sleep for delay
    Use deep sleep instead of a timer to delay between readings.
    There is a time delay after reading the temperature and it is
    not clear why it needs to be so long.
    
    On branch master
    modified:   user/app.c

--- user/app.c
+++ user/app.c
@@ -47,6 +47,7 @@ readTemp(void *arg)
         os_sprintf(mBuf, "%d.%03d degC", temperature, abs((mantissa * 625)/10));
     }
 
+    INFO("\r\n");
     INFO(mBuf);
     INFO("\r\n");
 
@@ -62,13 +63,38 @@ readTemp(void *arg)
 
 static os_timer_t read_timer;
 
+static void ICACHE_FLASH_ATTR
+user_deep_sleep(void)
+{
+    system_deep_sleep(5000000);
+}
+
+static void ICACHE_FLASH_ATTR
+sys_init_complete(void)
+{
+    readTemp(NULL);
+    os_timer_disarm(&read_timer);
+    os_timer_setfn(&read_timer, (os_timer_func_t *)user_deep_sleep, NULL);
+    // Allow time for the print buffers to flush
+    // PROBLEM: A short time seems to also cause a sync issue with the DS18B20
+    //          that is only fixed when the DS18B20 is power cycled.
+    //          50ms is too short. The symptom is that the reported
+    //          temperature is always 127.937.
+    //          200 - does not work
+    //          400 - does not work
+    //          500 - does work
+    //          It is not yet clear to me why 500ms is needed.
+    //
+    os_timer_arm(&read_timer, 500, 0);
+}
+
 //Init function 
 void ICACHE_FLASH_ATTR
 user_init()
 {
     uart_init(BIT_RATE_115200);
 
-    os_timer_disarm(&read_timer);
-    os_timer_setfn(&read_timer, (os_timer_func_t *)readTemp, (void *)0);
-    os_timer_arm(&read_timer, 5000, 1);
+    // system_deep_sleep() can not be called in user_init(), so use a
+    // timer function.
+    system_init_done_cb(sys_init_complete);
 }
